77cf0ee4072ef68d52c7ac8d7b2cab0967d5f82a
pinkovay <pedros.pinkovay@gmail.com>
2025-03-13 15:19:51 -0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,8 +11,12 @@ builder.Services.AddDbContext<DataContext>(opt => {
 });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 // builder.Services.AddOpenApi();
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+// Cross-origin resource sharing (CORS) is a security feature that allows browsers to request resources from other domains. - Basicamente estou autorizando endereços externos realizarem querys na minha API
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+
+
+  http = inject(HttpClient) // Aqui o estilo de injeção de dependencia pode ser tratado um pouco diferente, ao invés de colocar no construtor, parece bom seguir assim
+  title = 'DatingApp';
+  users: any;
+
+  // Basicamente o trabalho do Axios quando trabalhei no front do talkme com react. A principal diferença pe sintaxe apenas (sempre né)
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.info("The request was complete sucessfully")
+    })
+  } // Não precisa dar "unsubscribe" na requisição, pois toda requisição HTTP depois de concluida faz isso sozinha 
+}
